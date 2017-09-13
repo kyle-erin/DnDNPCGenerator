385520c69beda75672044f0784083033e0fa4026
Kyle Phillips <spaznur@gmail.com>
2017-09-13 13:17:19 -0700

    Added random traits via json

--- app/src/main/java/com/example/mi5trb/dndnpcgenerator/MainActivity.java
+++ app/src/main/java/com/example/mi5trb/dndnpcgenerator/MainActivity.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,14 +30,34 @@ public class MainActivity extends AppCompatActivity {
         //int test = R.attr.Races.
 
 
-
         //Methods
         btn_generate_npc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txt_rand1.setText(String.valueOf(new Random().nextInt(11)));
-                txt_rand2.setText(String.valueOf(new Random().nextInt(11)));
-                txt_rand3.setText(String.valueOf(new Random().nextInt(11)));
+                InputStream fis = getResources().openRawResource(R.raw.traits);
+                try {
+                    StringBuilder str = new StringBuilder();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+                    while (in.ready()) {
+                        str.append(in.readLine());
+                    }
+                    JSONObject jdata = new JSONObject(str.toString());
+                    JSONArray appearances = jdata.getJSONArray("appearances");
+                    JSONArray bonds = jdata.getJSONArray("bonds");
+                    JSONArray races = jdata.getJSONArray("races");
+
+                    Random r = new Random();
+                    int index = r.nextInt(appearances.length());
+                    txt_rand1.setText(appearances.getString(index));
+
+                    index = r.nextInt(bonds.length());
+                    txt_rand2.setText(bonds.getString(index));
+
+                    index = r.nextInt(races.length());
+                    txt_rand3.setText(races.getString(index));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
